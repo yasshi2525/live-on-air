1f3dd4eda895ea5c6dad902fee75732cdb4f1b0e
yasshi2525 <yasshy2@gmail.com>
2024-08-27 19:33:43 +0900

    ビルド結果に画像が含まれない問題を修正

---
+++ build.ts
@@ -0,0 +1,8 @@
+import { execSync } from 'child_process'
+import * as fs from 'node:fs'
+
+if (fs.existsSync('lib')) {
+  fs.rmSync('lib', { recursive: true })
+}
+execSync('tsc')
+fs.cpSync('image', 'lib/image')
