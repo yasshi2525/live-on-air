f88a664225f8a38e2e747e27ed226343126cce21
yasshi2525 <yasshy2@gmail.com>
2024-08-30 04:45:42 +0900

    layout のデバッグ用描画をテストコードに移動

--- spec/builder/layerBuilder.ts
+++ spec/builder/layerBuilder.ts
@@ -1,5 +1,25 @@
 import { LayerBuilder } from '../../src/builder/layerBuilder'
-import { LayerType } from '../../src/builder/layerConfig'
+import { LayerType, layerTypes } from '../../src/builder/layerConfig'
+import { image } from '../../src/util/loader'
+import { Layer } from '../../src/model/layer'
+
+const insertDebugView = (layer: Layer) => {
+  for (const typ of layerTypes) {
+    layer[typ].append(new g.FilledRect({
+      scene,
+      width: layer[typ].width,
+      height: layer[typ].height,
+      cssColor: '#888888',
+      opacity: 0.25
+    }))
+    layer[typ].append(new g.Sprite({
+      scene,
+      src: image(scene, `image/layer.${typ}.label.png`),
+      x: layer[typ].width / 2,
+      anchorX: 0.5
+    }))
+  }
+}
 
 describe('LayerBuilder', () => {
   it('デフォルトの設定で構築できる', () => {
@@ -9,6 +29,8 @@ describe('LayerBuilder', () => {
       expect(layer[typ]).toBeInstanceOf(g.E)
     }
     expect(scene.children).toEqual([layer.field])
+
+    insertDebugView(layer)
     gameContext.step()
     screenshot('layer-builder.default.png')
   })
@@ -18,6 +40,8 @@ describe('LayerBuilder', () => {
       .field({ x: 100, y: 100, width: 500, height: 500 })
       .build()
     expect(layer.field).toMatchObject({ x: 100, y: 100, width: 500, height: 500 })
+
+    insertDebugView(layer)
     gameContext.step()
     screenshot('layer-builder.custom.png')
   })
--- src/model/layer.ts
+++ src/model/layer.ts
@@ -1,5 +1,4 @@
 import { LayerConfig, LayerType } from '../builder/layerConfig'
-import { image } from '../util/loader'
 
 /**
  * ゲーム Scene 上にエンティティを前後適切に配置するためのレイヤ層.
@@ -21,20 +20,6 @@ export class LayerImpl implements Layer {
   }
 
   private createEntity (typ: LayerType) {
-    const parent = new g.E({ scene: this.scene, parent: this.scene, ...this.config[typ] })
-    parent.append(new g.FilledRect({
-      scene: this.scene,
-      width: parent.width,
-      height: parent.height,
-      cssColor: '#888888',
-      opacity: 0.25
-    }))
-    parent.append(new g.Sprite({
-      scene: this.scene,
-      src: image(this.scene, 'image/layer.field.label.png'),
-      x: parent.width / 2,
-      anchorX: 0.5
-    }))
-    return parent
+    return new g.E({ scene: this.scene, parent: this.scene, ...this.config[typ] })
   }
 }
