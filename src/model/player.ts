df64c320992a022ef2ae603576f5b8fc069fa5da
yasshi2525 <yasshy2@gmail.com>
2024-08-29 09:50:57 +0900

    playerのspotへの移動先設定機能の実装

--- src/model/player.ts
+++ src/model/player.ts
@@ -1,4 +1,17 @@
 import { Field } from './field'
+import { Spot } from './spot'
+/**
+ * プレイヤーの状態を表します.
+ *
+ * "non-field": マップ上に配置されていない.
+ *
+ * "staying": ある Spot に滞在中である.
+ *
+ * "moving": ある Spot に向かって移動中である.
+ *
+ * "stopping": マップ上で待機中である.
+ */
+export type PlayerStatus = 'non-field' | 'staying' | 'moving' | 'stopping'
 
 /**
  * プレイヤー.
@@ -6,6 +19,10 @@ import { Field } from './field'
  * プレイヤーはマップの上 (Field) を移動でき、 Spot を訪問すると生放送します.
  */
 export interface Player {
+  /**
+   * 移動速度. 1フレームで移動する距離 (画面座標系) で指定します
+   */
+  speed: number
   /**
    * プレイヤーの座標
    *
@@ -21,6 +38,25 @@ export interface Player {
   readonly field?: Field
 
   /**
+   * プレイヤーが現在訪問中の Spot.
+   *
+   * 訪問中出ない場合 undefined を返します.
+   */
+  readonly staying?: Spot
+
+  /**
+   * プレイヤーが現在向かっている Spot.
+   *
+   * 目的地がない場合、 undefined を返します
+   */
+  readonly destination?: Spot
+
+  /**
+   * プレイヤーの現在の状態を取得します.
+   */
+  readonly status: PlayerStatus
+
+  /**
    * 指定したマップに登録し、マップの上を移動できるようにします.
    *
    * 本メソッドを実行するとプレイヤーが画面上に描画されるようになります.
@@ -28,11 +64,48 @@ export interface Player {
    * @param field 登録先マップ
    */
   standOn(field: Field): void
+
+  /**
+   * 指定した Spot の場所にワープします.
+   *
+   * 移動速度 speed の制約は受けず、移動は瞬間で完了します.
+   * プレイヤーはマップ field を登録している必要があります
+   * @see standOn
+   *
+   * @param spot ワープ先
+   */
+  jumpTo(spot: Spot): void
+
+  /**
+   * 指定した Spot へ移動し始めます.
+   *
+   * 移動は 1フレームあたり speed の距離進みます.
+   * プレイヤーはマップ field を登録している必要があります
+   * @see standOn
+   *
+   * @param spot
+   */
+  departTo(spot: Spot): void
+
+  /**
+   * 移動中の場合、移動を中止します.
+   *
+   * 現在地で待機を開始します.
+   * プレイヤーはマップ field を登録している必要があります
+   * @see standOn
+   */
+  stop(): void
 }
 
 export class PlayerImpl implements Player {
   private _field?: Field
   private _location?: g.CommonOffset
+  private _staying?: Spot
+  private _destination?: Spot
+  private _status: PlayerStatus = 'non-field'
+
+  // eslint-disable-next-line no-useless-constructor
+  constructor (protected _speed: number) { }
 
   standOn (field: Field): void {
     if (this._field && this._field !== field) {
@@ -42,10 +115,86 @@ export class PlayerImpl implements Player {
 
     this._field = field
     this._location = { x: 0, y: 0 }
+    this._status = 'stopping'
 
     if (!field.player) {
       field.addPlayer(this)
     }
+  }
+
+  jumpTo (spot: Spot): void {
+    if (!spot.field || !spot.location) {
+      throw new Error('指定したspotがfieldに配置されていないためワープに失敗しました. spotをfieldに配置してください')
+    }
+    if (!this._field || !this._location) {
+      throw new Error('playerがfieldに配置されていないためワープに失敗しました. playerをfieldに配置してください')
+    }
+    if (this._field !== spot.field) {
+      throw new Error('指定したspotがplayerと異なるfieldに配置されているためワープに失敗しました. 同じfieldに配置されたspotを指定してください')
+    }
+    if (this._destination) {
+      throw new Error('playerは現在移動中のためワープに失敗しました. playerの移動を停止してください')
+    }
+
+    this._location.x = spot.location.x
+    this._location.y = spot.location.y
+
+    this._staying = spot
+    this._status = 'staying'
+  }
+
+  departTo (spot: Spot): void {
+    if (!spot.field) {
+      throw new Error('指定したspotがfieldに配置されていないため移動先の設定に失敗しました. spotをfieldに配置してください')
+    }
+    if (!this._field) {
+      throw new Error('playerがfieldに配置されていないため移動先の設定に失敗しました. playerをfieldに配置してください')
+    }
+    if (this._field !== spot.field) {
+      throw new Error('指定したspotがplayerと異なるfieldに配置されているため移動先の設定に失敗しました. 同じfieldに配置されたspotを指定してください')
+    }
+    if (this._destination) {
+      throw new Error('playerは現在移動中のため移動先の設定に失敗しました. playerの移動を停止してください')
+    }
+
+    this._field.disableSpotExcept(spot)
+
+    this._staying = undefined
+    this._destination = spot
+    this._status = 'moving'
+
+    if (spot.status !== 'target') {
+      spot.setAsDestination()
+    }
+  }
+
+  stop (): void {
+    if (!this._field) {
+      throw new Error('playerがfieldに配置されていないため移動の停止に失敗しました. playerをfieldに配置してください')
+    }
+    if (!this._destination) {
+      throw new Error('playerは現在移動中でないため移動の停止に失敗しました. playerの移動中に停止命令を実行してください')
+    }
+
+    const oldDestination = this._destination
+    this._field.enableSpotExcept(oldDestination)
+    this._destination = undefined
+    this._status = 'stopping'
+
+    if (oldDestination.status === 'target') {
+      oldDestination.unsetAsDestination()
+    }
+  }
+
+  get speed (): number {
+    return this._speed
+  }
+
+  set speed (value: number) {
+    if (value <= 0) {
+      throw new Error(`無効な値 "${value}" を移動速度に設定しようとしました. 0より大きな正の値を指定してください`)
+    }
+    this._speed = value
   }
 
   get location (): Readonly<g.CommonOffset> | undefined {
@@ -57,5 +206,17 @@ export class PlayerImpl implements Player {
 
   get field (): Field | undefined {
     return this._field
+  }
+
+  get destination (): Spot | undefined {
+    return this._destination
+  }
+
+  get staying (): Spot | undefined {
+    return this._staying
+  }
+
+  get status (): PlayerStatus {
+    return this._status
   }
 }
--- src/model/spot.ts
+++ src/model/spot.ts
@@ -1,5 +1,17 @@
 import { SpotImageConfig } from '../builder/spotConfig'
 import { Field } from './field'
+/**
+ * スポットの状態
+ *
+ * "non-deployed": マップ上に配置されていない.
+ *
+ * "enabled": プレイヤーが訪問可能である.
+ *
+ * "target": プレイヤーの目的地に設定されている.
+ *
+ * "disabled": プレイヤーの訪問を受け付けない.
+ */
+export type SpotStatus = 'non-deployed' | 'enabled' | 'target' | 'disabled'
 
 export interface Spot {
   /**
@@ -22,6 +34,11 @@ export interface Spot {
   readonly field?: Field
 
   /**
+   * Spot の状態を取得します.
+   */
+  readonly status: SpotStatus
+
+  /**
    * 指定したマップに登録します.
    *
    * 登録することで Player は Spot を訪問し、生放送できるようになります.
@@ -30,11 +47,32 @@ export interface Spot {
    * @param field 登録先のマップ
    */
   deployOn(field: Field): void
+
+  /**
+   * プレイヤーが目的地としていると設定します.
+   */
+  setAsDestination(): void
+
+  /**
+   * 目的地として設定されていることを解除します.
+   */
+  unsetAsDestination(): void
+
+  /**
+   * プレイヤーが目的地として選択できない状態にします.
+   */
+  disable(): void
+
+  /**
+   * プレイヤーが目的地として選択できる状態にします.
+   */
+  enable(): void
 }
 
 export class SpotImpl implements Spot {
   private _field?: Field
   private _location?: g.CommonOffset
+  private _status: SpotStatus = 'non-deployed'
 
   // eslint-disable-next-line no-useless-constructor
   constructor (public assets: Readonly<SpotImageConfig>) {}
@@ -47,10 +85,59 @@ export class SpotImpl implements Spot {
 
     this._field = field
     this._location = { x: 0, y: 0 }
+    this._status = 'enabled'
 
     if (!field.spots.some(sp => sp === this)) {
       field.addSpot(this)
     }
+  }
+
+  setAsDestination ():void {
+    if (!this._field) {
+      throw new Error('spotがfieldに配置されていないため移動先としての設定に失敗しました. spotをfieldに配置してください')
+    }
+    if (!this._field.player) {
+      throw new Error('playerがfieldに配置されていないため移動先としての設定に失敗しました. playerをfieldに配置してください')
+    }
+
+    this._status = 'target'
+
+    if (this._field.player.destination !== this) {
+      this._field.player.departTo(this)
+    }
+  }
+
+  unsetAsDestination (): void {
+    if (!this._field) {
+      throw new Error('spotがfieldに配置されていないため移動先設定の解除に失敗しました. spotをfieldに配置してください')
+    }
+    if (this._status !== 'target') {
+      throw new Error('spotは移動先として設定されていないため、移動先設定の解除に失敗しました.' +
+        ' 移動先として設定されているspotに対して解除命令を実行してください')
+    }
+    if (!this._field.player) {
+      throw new Error('playerがfieldに配置されていないため移動先としての設定に失敗しました. playerをfieldに配置してください')
+    }
+
+    this._status = 'enabled'
+
+    if (this._field.player.destination === this) {
+      this._field.player.stop()
+    }
+  }
+
+  enable (): void {
+    if (!this._field) {
+      throw new Error('spotがfieldに配置されていないため訪問先として指定可能にできませんでした. spotをfieldに配置してください')
+    }
+    this._status = 'enabled'
+  }
+
+  disable (): void {
+    if (!this._field) {
+      throw new Error('spotがfieldに配置されていないため訪問先として指定不可能にできませんでした. spotをfieldに配置してください')
+    }
+    this._status = 'disabled'
   }
 
   get field (): Field | undefined {
@@ -62,5 +149,9 @@ export class SpotImpl implements Spot {
       return { ...this._location }
     }
     return undefined
+  }
+
+  get status (): SpotStatus {
+    return this._status
   }
 }
