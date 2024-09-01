94e6b2063b556822a0d1cf761c0e71ecd4f28e2f
yasshi2525 <yasshy2@gmail.com>
2024-09-02 06:18:16 +0900

    awaitのつけ忘れ修正(gameClient.step())

--- spec/builder/layerBuilder.ts
+++ spec/builder/layerBuilder.ts
@@ -21,7 +21,7 @@ const insertDebugView = (layer: Layer) => {
 }
 
 describe('LayerBuilder', () => {
-  it('デフォルトの設定で構築できる', () => {
+  it('デフォルトの設定で構築できる', async () => {
     const layer = new LayerBuilder(scene).build()
     for (const typ of ['field'] as LayerType[]) {
       expect(layer[typ]).toBeDefined()
@@ -30,18 +30,18 @@ describe('LayerBuilder', () => {
     expect(scene.children).toEqual([layer.field])
 
     insertDebugView(layer)
-    gameContext.step()
+    await gameContext.step()
     screenshot('layer-builder.default.png')
   })
 
-  it('各レイヤの大きさをカスタマイズできる', () => {
+  it('各レイヤの大きさをカスタマイズできる', async () => {
     const layer = new LayerBuilder(scene)
       .field({ x: 100, y: 100, width: 500, height: 500 })
       .build()
     expect(layer.field).toMatchObject({ x: 100, y: 100, width: 500, height: 500 })
 
     insertDebugView(layer)
-    gameContext.step()
+    await gameContext.step()
     screenshot('layer-builder.custom.png')
   })
 })
