49e8ab10fd13c65e09dc4b5cb2b31d7906eecdc7
yasshi2525 <yasshy2@gmail.com>
2024-09-05 00:57:05 +0900

    ニコ生本番環境だと画像アセットがロードされない問題を暫定回避

--- src/util/loader.ts
+++ src/util/loader.ts
@@ -1,4 +1,4 @@
 // テスト時はモック化されるのでカバレッジ収集対象外
 // istanbul ignore next
 export const image = (container: g.Game | g.Scene, path: string): g.ImageAsset =>
-  (container instanceof g.Game ? container.scene()! : container).asset.getImage(require.resolve(`../../${path}`))
+  (container instanceof g.Game ? container.scene()! : container).asset.getImage(`/node_modules/@yasshi2525/live-on-air/${path}`)
